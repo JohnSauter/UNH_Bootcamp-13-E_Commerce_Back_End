9ecd7cc5b05c4b4433ff349d6a8f1f3eb7c490d5
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-11 16:10:01 -0400

    One route works

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,10 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const user_data = await Product.findAll();
+    res.status(200).json(user_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
