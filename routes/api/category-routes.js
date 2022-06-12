05347ee0cf8a06d071ad548140754ba90b616b1f
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-12 13:11:40 -0400

    Category routes work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const user_data = await Category.findAll({
-      include: [Product] 
+      include: [Product]
     });
     res.status(200).json(user_data);
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   try {
     const category_id = req.params.id;
     const user_data = await Category.findByPk(category_id, {
-      include: [Product] 
+      include: [Product]
     });
     if (user_data == null) {
       res.status(404).end();
@@ -34,16 +34,56 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this:
+   {
+    "category_name": "new category"
+   }
+   */
+  try {
+    const the_category = await Category.create(req.body);
+    res.status(201).json(the_category);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const user_data = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (user_data == null) {
+      res.status(404).end();
+    } else {
+      res.status(200).json(user_data);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category_id = req.params.id;
+    const user_data = await Category.findByPk(category_id);
+    if (user_data == null) {
+      res.status(404).end();
+    } else {
+      Category.destroy({
+        where: { id: category_id }
+      });
+      res.status(200).json(user_data);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
